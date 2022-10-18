438e024902cc2ec91f153ca54e2c4270195f4157
Evan Curtis <ewcurtis1997@gmail.com>
2022-10-18 12:41:34 -0400

    initial setup with webpack

---
+++ dist/main.js
@@ -0,0 +1,21 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+
+ function component() {
+    const element = document.createElement('div');
+ 
+   // Lodash, currently included via a script, is required for this line to work
+ 
+   // Lodash, now imported by this script
+    element.innerHTML = "Hello World!";
+ 
+    return element;
+  }
+ 
+  document.body.appendChild(component());
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EseUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9vZGluLXJlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJcbiBmdW5jdGlvbiBjb21wb25lbnQoKSB7XG4gICAgY29uc3QgZWxlbWVudCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoJ2RpdicpO1xuIFxuICAgLy8gTG9kYXNoLCBjdXJyZW50bHkgaW5jbHVkZWQgdmlhIGEgc2NyaXB0LCBpcyByZXF1aXJlZCBmb3IgdGhpcyBsaW5lIHRvIHdvcmtcbiBcbiAgIC8vIExvZGFzaCwgbm93IGltcG9ydGVkIGJ5IHRoaXMgc2NyaXB0XG4gICAgZWxlbWVudC5pbm5lckhUTUwgPSBcIkhlbGxvIFdvcmxkIVwiO1xuIFxuICAgIHJldHVybiBlbGVtZW50O1xuICB9XG4gXG4gIGRvY3VtZW50LmJvZHkuYXBwZW5kQ2hpbGQoY29tcG9uZW50KCkpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
