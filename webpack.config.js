27558dfbc838116df263b9ea2b99ff7c8d932a7d
Evan Curtis <ewcurtis1997@gmail.com>
2022-10-21 13:05:47 -0400

    change webpack to production

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
-
-  
+  mode: 'production',
+ 
   entry: './src/index.js',
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Odin Restaurant Page',
-    }),
-  ],
-
-  devtool: 'inline-source-map',
 
   output: {
     filename: 'main.js',
-    publicpath: '',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader',
-        "style-loader",
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: false,
-              publicPath: "",
-            },
-          }, 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
   },
 
 };
